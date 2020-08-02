cde3afd2b213db1537e6742b1c3200ba128979a8
Shaban <sh3ban.kamel@gmail.com>
2020-08-02 11:49:09 +0200

    Update pods

---
+++ App/AppTests/DiscoverMoviesVMTest.swift
@@ -0,0 +1,14 @@
+//
+// Created by Apple on 02/08/2020.
+// Copyright (c) 2020 sha. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import RxTest
+import RxBlocking
+@testable import SearchMoviesVM
+
+class DiscoverMoviesVMTest: XCTestCase {
+
+}
